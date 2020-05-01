261af0bab7fbaf752787201423631353a5a20bf5
Reinoptland <reinoptland@gmail.com>
2020-05-01 16:39:29 +0200

    Fetch data from api, set the state for movies

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,5 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function MovieList() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchMovies() {
+      const response = await axios.get(
+        `http://www.omdbapi.com/?s=indiana+jones&apikey=a7462395`
+      );
+
+      console.log(response.data.Search);
+      setMovies(response.data.Search);
+    }
+
+    fetchMovies();
+  }, []);
+
+  console.log("MOVIES", movies);
   return <div>I am a list</div>;
 }
