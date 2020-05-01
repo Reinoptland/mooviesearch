35b6fbd1adaa95dbc3b361c31edb03ded74fefe9
Reinoptland <reinoptland@gmail.com>
2020-05-01 16:57:12 +0200

    Get id from url using useParams hook

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,5 +1,9 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 export default function MovieDetails() {
+  const routeParameters = useParams();
+  console.log(routeParameters);
+
   return <div>DETAILS</div>;
 }
