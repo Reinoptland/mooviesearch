05ff5302cad7690c5ca1869f4ca87bccdb4f2a2e
Reinoptland <reinoptland@gmail.com>
2020-05-01 16:00:56 +0200

    Add NavLink styling and set route / as exact

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+} from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <nav>
-          <Link to="/">Home</Link>
-          <Link to="/movies">Movies</Link>
+          <NavLink
+            activeStyle={{
+              fontWeight: "bold",
+              color: "red",
+            }}
+            to="/"
+            exact
+          >
+            Home
+          </NavLink>
+          <NavLink
+            activeStyle={{
+              fontWeight: "bold",
+              color: "green",
+            }}
+            to="/movies"
+          >
+            Movies
+          </NavLink>
         </nav>
         <header className="App-header">
           <Switch>
             <Route path="/movies">
               <h1>WATCH MOVIES MAN!</h1>
             </Route>
-            <Route path="/">
+            <Route exact path="/">
               <h1>WELCOME TO MY APP, please stay a while</h1>
             </Route>
           </Switch>
